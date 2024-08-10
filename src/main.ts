88893ab393b6f6a557fe2b6e13191bf2865f67c1
Marek Hakala <hakala.marek@gmail.com>
2024-08-10 12:43:08 -0400

    Added a transaction that fails

--- src/main.ts
+++ src/main.ts
@@ -27,3 +27,41 @@ await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
 // get the initial state of Square after deployment
 const num0 = zkAppInstance.num.get();
 console.log('state after init:', num0.toString());
+
+// ----------------------------------------------------
+
+const txn1 = await Mina.transaction(senderAccount, async () => {
+  await zkAppInstance.update(Field(9));
+});
+await txn1.prove();
+await txn1.sign([senderKey]).send();
+
+const num1 = zkAppInstance.num.get();
+console.log('state after txn1:', num1.toString());
+
+// ----------------------------------------------------
+
+try {
+  const txn2 = await Mina.transaction(senderAccount, async () => {
+    await zkAppInstance.update(Field(75));
+  });
+  await txn2.prove();
+  await txn2.sign([senderKey]).send();
+} catch (error: any) {
+  console.log(error.message);
+}
+const num2 = zkAppInstance.num.get();
+console.log('state after txn2:', num2.toString());
+
+// ----------------------------------------------------
+
+const txn3 = await Mina.transaction(senderAccount, async () => {
+  await zkAppInstance.update(Field(81));
+});
+await txn3.prove();
+await txn3.sign([senderKey]).send();
+
+const num3 = zkAppInstance.num.get();
+console.log('state after txn3:', num3.toString());
+
+// ----------------------------------------------------
